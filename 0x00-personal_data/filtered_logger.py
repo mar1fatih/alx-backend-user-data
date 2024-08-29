c0a6c731602465494308664033f82f0e5c153e8e
mar1fatih <marouanefatih631@gmail.com>
2024-08-29 18:10:04 +0100

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ returns the log message obfuscated """
     for field in fields:
         pattern = '{}=[^{}]+'.format(field, separator)
-        text = '{}={}'.format(field, redaction)
-        message = re.sub(pattern, text, message)
+        message = re.sub(pattern, '{}={}'.format(field, redaction), message)
     return message
