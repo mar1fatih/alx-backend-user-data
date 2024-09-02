a97180eb249ffe5257e0667deb60bc1dc4dd749b
mar1fatih <marouanefatih631@gmail.com>
2024-09-02 17:50:52 +0100

    adding 403 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> None:
     """ raise a 401 error """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ raise a 403 error """
+    abort(403)
