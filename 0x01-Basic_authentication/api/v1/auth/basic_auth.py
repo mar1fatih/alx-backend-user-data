96b490c6ab6623dbebf3a912278178cd651391c2
mar1fatih <marouanefatih631@gmail.com>
2024-09-05 00:40:26 +0100

    addid decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode by base64"""
+        b64_header = base64_authorization_header
+        if not b64_header or type(b64_header) is not str:
+            return None
+        try:
+            b64_header = base64.b64decode(b64_header).decode('utf-8')
+        except Exception:
+            b64_header = None
+        return b64_header
