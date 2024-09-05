119e7e54d4a55e073615814f00b8bdbc0f2d67ca
mar1fatih <marouanefatih631@gmail.com>
2024-09-05 17:55:51 +0100

    adding to SessionAuth class create_session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,12 @@ class SessionAuth(Auth):
         """creates a Session ID for a user_id"""
         if not user_id or type(user_id) is not str:
             return None
-        session_id = uuid4()
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
