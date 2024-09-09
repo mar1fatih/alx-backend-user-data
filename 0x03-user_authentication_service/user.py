2f98faceb24af252022f3089f70dc024bc52d3fc
mar1fatih <marouanefatih631@gmail.com>
2024-09-09 16:42:16 +0100

    task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""user table"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Column
+
+
+Base = declarative_base()
+
+class User(Base):
+    """ORM for user table"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String)
+    reset_token = Column(String)
