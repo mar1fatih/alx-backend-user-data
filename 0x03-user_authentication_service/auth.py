622a66eb999d5c9fa58c155079eaacfb42eb7fed
mar1fatih <marouanefatih631@gmail.com>
2024-09-10 23:38:11 +0100

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,7 +72,7 @@ class Auth:
         """updates the corresponding user’s session ID to None"""
         try:
             usr = self._db.find_user_by(id=user_id)
-            self._db.update_user(user_id, usr.session_id=None)
+            self._db.update_user(user_id, session_id=None)
             return None
         except NoResultFound or InvalidRequestError:
             return None
